354b0d3f795b841021bbd18712dbeb0e1bc31df6
lksprado <lks-prado@live.com>
2025-01-16 18:22:01 -0300

    teste

--- app/file_processing/parameters_maker.py
+++ app/file_processing/parameters_maker.py
@@ -1,24 +1,26 @@
 # THE SCRIPTS IS TO GATHER DATA FROM THE FIRST FILES TO APPLY THEM AS PARAMETERS IN THE API
 
 import polars as pl 
-def get_column(param_name:str,col: str, csv_filename:str):
-    
+
+def get_column(param_name: str, col: str, csv_filename: str):
     df = pl.read_csv(csv_filename)
-    unique_values = set(df[col])
+    unique_values = set(map(str, df[col].to_list()))  # Convert values to string using map
     new_df = pl.DataFrame({
-        col: list(unique_values),
-        "api_parameter": [param_name] * len(unique_values)
+        "api_parameter": [param_name] * len(unique_values),
+        "value": list(unique_values)
     })
     return new_df
-    #new_df.write_csv(f'{output_dir}/{param_name}.csv')
 
 ###########################################################################
 
 csv_file = 'data/csv_data/raw/raw_seasons.csv'
-df1 = get_column('season_id','season_id', csv_file)
+df1 = get_column('season_id', 'season_id', csv_file)
 
-csv_file = 'data/csv_data/raw/raw_seasons.csv'
-df2 = get_column('team_id','triCode', csv_file)
+csv_file = 'data/csv_data/raw/raw_teams.csv'
+df2 = get_column('team_id', 'triCode', csv_file)
+
+df3 = pl.concat([df1, df2])
+df3.write_csv('data/csv_data/parameters.csv')
 
 
 ## PARAMETERS
